add143230e4e50112361ef4827e3dfe3818b0a3d
unknown <richane000@warren.k12.in.us>
2022-02-01 12:26:37 -0500

    Initial commit

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length , double  width){
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
+
