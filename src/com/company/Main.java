cb54357e10af5e1e4fc2509319a7ffd5f5ba3fad
richane000 <richane000@warren.k12.in.us>
2022-02-07 12:03:12 -0500

    work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import org.w3c.dom.css.CSSPrimitiveValue;
+import org.w3c.dom.css.Rect;
+
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        Rectangle rect1 = new Rectangle(2, 4);
+
+        System.out.println("Look at the rectangle");
+
+        rect1.setWidth(5);
+        rect1.setLength(2);
+
+        System.out.println("The width is 5 and the length is 2");
 
     }
 }
